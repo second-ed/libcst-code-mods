2d9a2bd0a36665f7aa2ffdd5c2e474815ae6f964
ed cuss <edcussmusic@gmail.com>
2026-02-14 10:01:49 +0000

    fix: allow for matcher transforms too

--- src/libcst_code_mods/apply.py
+++ src/libcst_code_mods/apply.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from collections.abc import Callable
 
 import libcst as cst
@@ -8,14 +10,16 @@ from libcst_code_mods.node_collector import NodeCollector
 from libcst_code_mods.transformer import ApplyTransformations, NodeTransform
 
 
-def apply_code_mod(
+def apply_code_mod(  # noqa: PLR0913
     code: str,
-    matcher: m.BaseMatcherNode,
+    collecter_matcher: m.BaseMatcherNode,
     filter_fns: tuple[Callable[..., bool]],
     transform_fn: Callable[[cst.CSTNode], cst.CSTNode],
+    transform_matcher: m.BaseMatcherNode | None = None,
+    transform_matcher_fn: Callable[[cst.CSTNode], cst.CSTNode] | None = None,
 ) -> str:
     wrapper = cst.MetadataWrapper(code)
-    collector = NodeCollector(matcher)
+    collector = NodeCollector(collecter_matcher)
     wrapper.visit(collector)
 
     filtered = (
@@ -24,4 +28,6 @@ def apply_code_mod(
         else collector.results
     )
     replacements = {elem.node: NodeTransform(transform_fn) for elem in filtered}
-    return wrapper.module.visit(ApplyTransformations(replacements)).code
+    return wrapper.module.visit(
+        ApplyTransformations(replacements, matcher=transform_matcher, transform_matcher_fn=transform_matcher_fn)
+    ).code
