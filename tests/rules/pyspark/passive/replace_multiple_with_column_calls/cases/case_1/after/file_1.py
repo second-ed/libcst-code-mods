ae1ee20e6aad3df41a9dd8f9ce77b122e2765a67
ed cuss <edcussmusic@gmail.com>
2026-06-16 19:55:47 +0100

    test: more withColumns cases

--- tests/rules/pyspark/passive/replace_multiple_with_column_calls/cases/case_1/after/file_1.py
+++ tests/rules/pyspark/passive/replace_multiple_with_column_calls/cases/case_1/after/file_1.py
@@ -1,2 +1,22 @@
 def two_with_column_calls_in_a_row() -> None:
     df.withColumns({"a": lit(1), "b": col("blah")})
+
+
+def three_with_column_calls_in_a_row() -> None:
+    df.withColumns({"a": lit(1), "b": col("blah"), "c": lit(3)})
+
+
+def with_column_chain_assigned() -> None:
+    result = df.withColumns({"a": lit(1), "b": col("blah"), "c": upper(col("name"))})
+
+
+def with_column_chain_after_filter() -> None:
+    df.filter(col("active")).withColumns({"a": lit(1), "b": col("blah")})
+
+
+def with_column_chain_inside_return() -> None:
+    return df.withColumns({"a": lit(1), "b": col("blah"), "c": concat(col("x"), col("y"))})
+
+
+def with_column_chain_followed_by_select() -> None:
+    df.withColumns({"a": lit(1), "b": col("blah")}).select("a", "b")
--- tests/rules/pyspark/passive/replace_multiple_with_column_calls/cases/case_1/before/file_1.py
+++ tests/rules/pyspark/passive/replace_multiple_with_column_calls/cases/case_1/before/file_1.py
@@ -1,2 +1,22 @@
 def two_with_column_calls_in_a_row() -> None:
     df.withColumn("a", lit(1)).withColumn("b", col("blah"))
+
+
+def three_with_column_calls_in_a_row() -> None:
+    df.withColumn("a", lit(1)).withColumn("b", col("blah")).withColumn("c", lit(3))
+
+
+def with_column_chain_assigned() -> None:
+    result = df.withColumn("a", lit(1)).withColumn("b", col("blah")).withColumn("c", upper(col("name")))
+
+
+def with_column_chain_after_filter() -> None:
+    df.filter(col("active")).withColumn("a", lit(1)).withColumn("b", col("blah"))
+
+
+def with_column_chain_inside_return() -> None:
+    return df.withColumn("a", lit(1)).withColumn("b", col("blah")).withColumn("c", concat(col("x"), col("y")))
+
+
+def with_column_chain_followed_by_select() -> None:
+    df.withColumn("a", lit(1)).withColumn("b", col("blah")).select("a", "b")
