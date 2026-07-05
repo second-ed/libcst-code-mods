1c8db4e4c5e461b39800965e2a7bdb0c0f100af2
ed cuss <edcussmusic@gmail.com>
2026-07-05 13:10:57 +0100

    test: add test case for nested function calls

--- tests/rules/general/passive/replace_multiple_function_calls_in_comp_with_walrus/cases/case_1/before/file_1.py
+++ tests/rules/general/passive/replace_multiple_function_calls_in_comp_with_walrus/cases/case_1/before/file_1.py
@@ -8,3 +8,9 @@ def should_replace_multiple_function_calls_with_walrus(xs: Iterable, f: Callable
 
 def should_replace_multiple_function_calls_with_comparison_with_walrus(xs: Iterable, f: Callable) -> list[Any]:
     return [f(x) for x in xs if f(x) > 1]
+
+
+def should_replace_multiple_function_calls_with_nested_call_in_cond(
+    xs: Iterable, f: Callable, g: Callable
+) -> list[Any]:
+    return [f(x) for x in xs if g(f(x))]
